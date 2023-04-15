a0690325218072ab814f32b318788de0d0a7a60b
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-04-15 15:04:17 +0300

    1

--- app/home/_layout.js
+++ app/home/_layout.js
@@ -11,7 +11,8 @@ export default () => {
           options={{
             tabBarIcon: ({ color }) => (
               <FontAwesome name="home" size={30} color={color} />
-            ),
+              ),
+              title: 'Feed'
           }}
         />
         <Tabs.Screen
@@ -19,7 +20,8 @@ export default () => {
           options={{
             tabBarIcon: ({ color }) => (
               <FontAwesome name="search-plus" size={30} color={color} />
-            ),
+              ),
+              titlte: 'Search'
           }}
         />
         <Tabs.Screen
@@ -27,7 +29,8 @@ export default () => {
           options={{
             tabBarIcon: ({ color }) => (
               <Entypo name="message" size={30} color={color} /> 
-            ),
+              ),
+              title: 'Messages'
           }}
         />
       </Tabs>
